7ee945062995e342a7a383dc9214bad17769eab8
joshi1999 <joshi1999@yahoo.de>
2014-07-06 10:52:16 +0200

    Added pre-end of Game

--- Mathespiel/src/tk/joshi1999/source/source/main/main.java
+++ Mathespiel/src/tk/joshi1999/source/source/main/main.java
@@ -11,53 +11,61 @@ public class main
 		int l;
 		int l1;
 		int Points = 0;
+		boolean Fehler = false;
 		
 		
 		
 		while(true)
 		{
-		int a = (int) (Math.random()*10+1);
-		int b = (int) (Math.random()*10+1);
-		
-		l1 = a*b;
-		
-		
-		
-		String s = javax.swing.JOptionPane.showInputDialog( a+"*"+b+"= ");
-		l = Integer.parseInt(s);
-		
-		
-		
-		
-		if(l == l1)
-		{
-			javax.swing.JOptionPane.showInputDialog("Richtig!");
-			Points++;
-		}
-		else
-		{
+			
+			
+			if(points == 10)
 			break;
-		}
+			
+			int a = (int) (Math.random()*10+1);
+			int b = (int) (Math.random()*10+1);
+			
+			l1 = a*b;
+		
+		
+		
+			String s = javax.swing.JOptionPane.showInputDialog( a+"*"+b+"= ");
+			l = Integer.parseInt(s);
+			
+		
+		
+		
+			if(l == l1)
+			{
+				javax.swing.JOptionPane.showInputDialog("Richtig!");
+				Points++;
+			}
+			else
+			{
+				Fehler = true;
+				break;
+			}
 		
 		
 		}
 		 
-		   JFrame fenster = new JFrame("Mathe");
+		JFrame fenster = new JFrame("Mathe");
 		    
 		   
-		   fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 	
 		   
-		   
-		   JLabel label = new JLabel("Du hast einen Fehler gemacht! \nDeine Punkte: " + Points, JLabel.CENTER);
-		 
+		if(Fehler == true)   
+		JLabel label = new JLabel("Du hast einen Fehler gemacht! \nDeine Punkte: " + Points, JLabel.CENTER);
+		else
+		JLabel label = new JLabel("Du hast volle 10 Punkte!", JLabel.CENTER);
 		  
-		   fenster.getContentPane().add(label);
+		fenster.getContentPane().add(label);
 		 
 		   
-		   fenster.setSize(300, 200);
+		fenster.setSize(300, 200);
 		 
 		   
-		   fenster.setVisible(true);
+		fenster.setVisible(true);
 	}
 }
